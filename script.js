7514bde6130ac3dd187776a307291c59861c6357
Your name <Your email>
2024-09-24 19:02:55 +0200

    front

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-// test
+/*
+document.querySelector("#search").addEventListener('click' , function () {
+    fetch('') 
+    .then(response => response.json())
+    .then(data => {
+        if(!data){
+            document.querySelector("#content-right").innerHTML = 
+            `<div id="content-right">
+                <div id="train"><img src="images/notfound.png" class="train"></div>
+                <div><p>No trip found</p></div>
+            </div>`
+        } else if (data){
+            for (let i=0;i<data.length;i++){
+            document.querySelector("#content-right").innerHTML = 
+            ``
+            }
+        }
+    })
+})
+*/
+
+document.querySelector("#search").addEventListener('click' , function () {
+    document.querySelector("#content-right").innerHTML = 
+    `<div id="content-right">
+        <div id="train"><img src="images/notfound.png" class="train"></div>
+        <div><p>No trip found</p></div>
+    </div>`
+}
+)
+
+
