ab628971c3ef412f5bb63a08a3264f1f42c15b4f
Your name <Your email>
2024-09-25 12:15:56 +0200

    front

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-/*
+
 document.querySelector("#search").addEventListener('click' , function () {
-    fetch('') 
-    .then(response => response.json())
+    const departure = document.querySelector('#departure').value
+    const arrival = document.querySelector("#arrival").value
+    fetch('http://localhost:3000/trips')
+    .then(response =>  response.json())
     .then(data => {
-        if(!data){
-            document.querySelector("#content-right").innerHTML = 
-            `<div id="content-right">
-                <div id="train"><img src="images/notfound.png" class="train"></div>
-                <div><p>No trip found</p></div>
-            </div>`
-        } else if (data){
-            for (let i=0;i<data.length;i++){
-            document.querySelector("#content-right").innerHTML = 
-            ``
-            }
-        }
+           console.log(data)
     })
 })
-*/
 
+
+/*
 document.querySelector("#search").addEventListener('click' , function () {
     document.querySelector("#content-right").innerHTML = 
-    `<div id="content-right">
-        <div id="train"><img src="images/notfound.png" class="train"></div>
-        <div><p>No trip found</p></div>
-    </div>`
+    `<div id="train"><img src="images/notfound.png" class="train"></div>
+     <div><p>No trip found</p></div>`
     }
 )
-
-
+*/
